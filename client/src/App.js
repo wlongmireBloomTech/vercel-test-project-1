09b0fbad86217c15baf922e69574de8c986ea4c1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-06-17 13:26:19 -0400

    moved into client.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>WARREN'S BIG MONEY MAKING IDEA</h1>
+      <button>CLICK TO GIVE ME A BAG OF MONEY</button>
+      <h3>Here is an update</h3>
+    </main>
+  );
+}
+
+export default App;
